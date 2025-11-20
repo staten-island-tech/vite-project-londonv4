f3a7be87aec6fb4749bec76c0eedb8538c2ec823
londonv4 <londonv4@nycstudents.net>
2025-11-20 09:31:38 -0800

    n

---
+++ .js
@@ -0,0 +1,18 @@
+const gallery = [
+{name: "", album: "",  }
+{name: "", album: "",  }
+]
+
+
+Digital Art Gallery- You will create a site to view art or photos or whatever
+aesthetically pleasing thing makes sense to you. Allow users to filter the
+images.
+Project must include the following.
+● Preselected image gallery which can be filtered or changed
+
+● Users can click on an image to generate a pop-up modal to learn
+more information
+● Upload new images
+● Random art of the day header
+● Light mode/ dark mode or theming in general
+
