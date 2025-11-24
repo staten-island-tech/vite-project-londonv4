82e3fa5de117559c21588c38c892e7daced9212a
londonv4 <londonv4@nycstudents.net>
2025-11-24 12:32:01 -0500

    hi

--- .js
+++ .js
@@ -1,8 +1,53 @@
 const gallery = [
-{name: "", album: "",  }
-{name: "", album: "",  }
-]
-
+  {
+    name: "Ariana Grande",
+    url: "",
+    category: "Yours Truly",
+    alt: "Ariana Grande"
+  },
+  {
+    name: "Ariana Grande",
+    url: "",
+    category: "My Everything",
+    alt: "Ariana Grande"
+  },
+  {
+    name: "Ariana Grande",
+    url: "",
+    category: "Dangerous Woman",
+    alt: "Ariana Grande"
+  },
+  {
+    name: "Ariana Grande",
+    url: "",
+    category: "Sweetenert",
+    alt: "Ariana Grande"
+  },
+  {
+    name: "Ariana Grande",
+    url: "",
+    category: "Thank U, Next",
+    alt: "Ariana Grande"
+  },
+  {
+    name: "Ariana Grande",
+    url: "",
+    category: "Positions",
+    alt: "Ariana Grande"
+  },
+  {
+    name: "Ariana Grande",
+    url: "",
+    category: "Eternal Sunshine",
+    alt: "Ariana Grande"
+  },
+  {
+    name: "Ariana Grande",
+    url: "",
+    category: "Wicked",
+    alt: "Ariana Grande"
+  }
+];
 
 Digital Art Gallery- You will create a site to view art or photos or whatever
 aesthetically pleasing thing makes sense to you. Allow users to filter the
